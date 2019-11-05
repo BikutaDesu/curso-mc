14197331e6c411ca54e6c6dc88f827470d7e008a
Victor Neves <victor.neves4@fatec.sp.gov.br>
2019-11-05 07:28:55 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/bikuta/cursomc/domain/Categoria.java
+++ src/main/java/com/bikuta/cursomc/domain/Categoria.java
@@ -2,10 +2,18 @@ package com.bikuta.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // Serializable - usada quando os objetos dessa classe podem ser transformados em um sequencia de bytes
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
