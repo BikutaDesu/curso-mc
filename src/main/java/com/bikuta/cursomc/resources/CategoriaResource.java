c25eaad0d1e976abc9fde89e1e5ec8c53b3aae2f
Victor Neves <victor.neves4@fatec.sp.gov.br>
2020-05-06 03:27:06 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/bikuta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/bikuta/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.bikuta.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bikuta.cursomc.domain.Categoria;
+import com.bikuta.cursomc.dto.CategoriaDTO;
 import com.bikuta.cursomc.services.CategoriaService;
 
 @RestController
@@ -52,5 +55,17 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		//.stream() percorre todos os itens da lista
+		//.map realiza uma função para cada item da lista
+		//obj -> função anonima (obj é o argumento da função anônima)
+		//após isso é necessário voltar o stream de objetos para uma lista
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
 
 }
